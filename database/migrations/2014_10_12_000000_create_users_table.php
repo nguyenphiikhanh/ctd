487dd0de91d38415aab65ac4f094db8b5211bcc6
Khanh_dev <nguyenphiikhanh@gmail.com>
2023-01-01 19:06:21 +0700

    #CONGTACDOAN-1 auth settings

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('ho');
+            $table->string('ten');
+            $table->string('email')->unique()->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->tinyInteger('role');
+            $table->integer('id_class')->nullable();
+            $table->integer('id_khoa')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
