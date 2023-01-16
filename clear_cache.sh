08c5f7d1a594aaeede72b830167bc8f8a35c67fd
dev_khanhnp <nguyenphiikhanh@gmail.com>
2023-01-16 13:56:56 +0700

    #deployment-branch add deploy scripts

---
+++ clear_cache.sh
@@ -0,0 +1,16 @@
+echo "Cong Tac Doan - Dev Environment Deploying..."
+
+echo "pulling from origin..."
+git fetch --all && git reset --hard origin/CONGTACDOAN-deployment && git pull
+
+echo "Database migrating..."
+php artisan migrate
+
+echo "production UI building..."
+npm run prod
+
+echo "optimzing..."
+php artisan optimize:clear
+
+echo "Changing Owner"
+sudo chown -R www-data:www-data /www/wwwroot/congtacdoan-test-env.nguyenphikhanh.dev/
