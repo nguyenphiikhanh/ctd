f6e5d413acfbe7d53f7be8d5e385e0bc82adb235
Dev. Khanh <nguyenphiikhanh@gmail.com>
2023-02-11 09:46:11 +0700

    #CONGTACDOAN-5 change webpack setting

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+ mix.js('resources/js/app.js', 'public/js')
+ .sass('resources/sass/app.scss', 'public/css');
+
  console.log("****Building for " + process.env.MIX_APP_ENV);
 
  if(mix.inProduction()){
@@ -19,3 +22,4 @@ const mix = require('laravel-mix');
      mix.sourceMaps();
  }
  mix.disableNotifications();
+
