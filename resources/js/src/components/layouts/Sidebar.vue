8bc7585a79a133f992b733c1e806eb680bd67d74
Dev. Khanh <nguyenphiikhanh@gmail.com>
2023-03-04 02:01:57 +0700

    #CONGTACDOAN-6 chore: sidebar item

--- resources/js/src/components/layouts/Sidebar.vue
+++ resources/js/src/components/layouts/Sidebar.vue
@@ -83,6 +83,14 @@
                     </a>
                 </li><!-- .nk-menu-item -->
             </router-link>
+            <router-link v-if="user.role == roles.ROLE_BI_THU_DOAN" :to="{name: 'NvspPoint'}">
+                <li class="nk-menu-item">
+                    <a href="#" class="nk-menu-link">
+                        <span class="nk-menu-icon"><em class="icon ni ni-todo-fill"></em></span>
+                        <span class="nk-menu-text">Quản lý điểm Tự đánh giá</span>
+                    </a>
+                </li><!-- .nk-menu-item -->
+            </router-link>
 
             <router-link v-if="user.role == roles.ROLE_LOP_TRUONG || user.role == roles.ROLE_CBL || user.role == roles.ROLE_SINH_VIEN" :to="{name: 'Notifications'}">
                 <li class="nk-menu-item">
