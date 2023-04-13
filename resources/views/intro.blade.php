2816a7ac90940164957c010488aad9916c74d25b
Dev. Khanh <nguyenphiikhanh@gmail.com>
2023-04-14 00:27:19 +0700

    #CONGTACDOAN-6 fix: intro favico

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name') }}</title>
-
+        <!-- Fav Icon  -->
+        <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
